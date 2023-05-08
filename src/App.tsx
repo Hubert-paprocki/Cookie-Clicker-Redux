26d7d7845e13dd0db98d6e418f3d5afc3312fa06
X-Tripen <bnhp@op.pl>
2023-05-08 17:28:40 +0200

    feat:add app background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Footer from "./components/Footer";
+import bg from "./images/CookieBg.jpg";
 
 function App() {
   return (
     <div className="bg-stone-800">
-      <div className="bg-stone-800 min-h-screen"></div> <Footer />
+      <div className="min-h-screen relative">
+        <div
+          className="absolute inset-0 opacity-40 animate-pulse"
+          style={{ backgroundImage: `url(${bg})` }}
+        ></div>
+        <div className="absolute inset-0 e text-white">test</div>
+      </div>
+      <Footer />
     </div>
   );
 }
