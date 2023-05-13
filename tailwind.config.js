ccb523651d3b6bca1d70c1df1457dea904e42431
X-Tripen <bnhp@op.pl>
2023-05-13 15:36:03 +0200

    feat:add animation to title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         Braah: ["Darumadrop One", "cursive"],
+      },
+      keyframes: {
+        pulse2: {
+          "0%, 100%": { transform: "scale(100%) " },
+          "50%": { transform: "scale(105%) rotate(-1deg)" },
+        },
+      },
+      animation: {
+        pulse2: "pulse2 1s ease-in-out infinite",
       },
     },
   },
