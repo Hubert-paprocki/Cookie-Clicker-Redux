3bc1e7cba4062d9216f479da93e217ca81156d70
X-Tripen <bnhp@op.pl>
2023-05-23 10:52:37 +0200

    fix: history string splitting

--- src/components/AppBox/AppBox-Game/UserStatsBox/UserActionHistory.tsx
+++ src/components/AppBox/AppBox-Game/UserStatsBox/UserActionHistory.tsx
@@ -6,26 +6,45 @@ function UsersActionHistory(): JSX.Element {
   const history = useAppSelector((state) => state.cookie.history);
 
   const splitItemString = (item: string) => {
-    const index = item.indexOf("cookie");
-    if (index !== -1) {
-      const part1 = item.substring(0, index);
-      const part2 = item.substring(index + "cookie".length);
+    const index1 = item.indexOf("cookie");
+    const index2 = item.indexOf("(");
+    if (index1 !== -1 && index2 !== -1) {
+      const part1 = item.substring(0, index1);
+      const part2 = item.substring(index1 + "cookie".length, index2);
+      const part3 = item.substring(index2);
+      return [part1, part2, part3];
+    } else if (index1 !== -1) {
+      const part1 = item.substring(0, index1);
+      const part2 = item.substring(index1 + "cookie".length);
       return [part1, part2];
     }
     return [item];
   };
 
   return (
-    <div>
+    <div className="grow ml-2">
       <h2>— History —</h2>
       <ul>
         {history.map((item, index) => {
-          const [part1, part2] = splitItemString(item);
+          const [part1, part2, part3] = splitItemString(item);
           return (
-            <li key={index} className="flex items-center justify-start ">
+            <li key={index} className="flex items-center justify-start nowrap">
               <p className="my-1">{part1}</p>
-              <img src={CookieSvg} alt="cookie" className="w-7 mx-1" />
-              <p className="my-1 ml-6">{part2}</p>
+              {part2 && (
+                <>
+                  <img
+                    src={CookieSvg}
+                    alt="cookie"
+                    className="w-7 mx-2 mt-1.5"
+                  />
+                  <p className="my-1">{part2}</p>
+                </>
+              )}
+              {part3 ? (
+                <p className="my-1 ml-3">{part3}</p>
+              ) : (
+                <img src={CookieSvg} alt="cookie" className="w-7 mx-2 mt-1.5" />
+              )}
             </li>
           );
         })}
