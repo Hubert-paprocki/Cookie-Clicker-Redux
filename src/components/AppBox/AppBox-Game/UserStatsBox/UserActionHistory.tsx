251a48c351a15077243e9efa5ef6d0d59dd4bd1d
X-Tripen <bnhp@op.pl>
2023-05-25 12:55:50 +0200

    refactor:rollback history name changes

--- src/components/AppBox/AppBox-Game/UserStatsBox/UserActionHistory.tsx
+++ src/components/AppBox/AppBox-Game/UserStatsBox/UserActionHistory.tsx
@@ -14,18 +14,21 @@ const UsersActionHistory = (): JSX.Element => {
 
   const getDetailsMakeStrings = (item: HistoryItem): [string, string] => {
     if (item.type === "earn") {
-      const earnedCookies = `${language.history.earn} ${item.earnedCookiesVal}`;
-      const cookieValue = `(${item.cookieVal})`;
-      return [earnedCookies, cookieValue];
+      return [
+        `${language.history.earn} ${item.earnedCookiesVal}`,
+        `(${item.cookieVal})`,
+      ];
     } else if (item.type === "autoEarn") {
-      const autoEarnedCookies = `${language.history.autoEarn[0]} ${item.earnedCookiesVal}`;
-      const interval = `${language.history.autoEarn[1]} ${item.interval}"`;
-      return [autoEarnedCookies, interval];
+      return [
+        `${language.history.autoEarn[0]} ${item.earnedCookiesVal}`,
+        `${language.history.autoEarn[1]} ${item.interval}"`,
+      ];
     } else if (item.type === "boughtItem" && item.boosterId) {
       const boosterName = boosters[item.boosterId]?.name || "";
-      const boughtItem = `${language.history.boughtItem[0]} ${boosterName}`;
-      const price = `${language.history.boughtItem[1]} ${item.price}`;
-      return [boughtItem, price];
+      return [
+        `${language.history.boughtItem[0]} ${boosterName}`,
+        `${language.history.boughtItem[1]} ${item.price}`,
+      ];
     }
     return ["", ""];
   };
@@ -35,20 +38,19 @@ const UsersActionHistory = (): JSX.Element => {
       <h2>{language.userStats?.history}</h2>
       <ul>
         {recentHistory.map((item, index) => {
-          const [actionDescription, actionDetails] =
-            getDetailsMakeStrings(item);
+          const [part1, part2] = getDetailsMakeStrings(item);
           return (
             <li
               key={index}
               className="flex items-center justify-center md:justify-start nowrap"
             >
-              <p className="my-1 whitespace-nowrap">{actionDescription}</p>
+              <p className="my-1 whitespace-nowrap">{part1}</p>
               <img
                 src={CookieSvg}
                 alt="cookie"
                 className="w-5 sm:w-6 md:w-7 mx-1 md:mx-2 mt-1 md:mt-1.5"
               />
-              <p className="my-1 whitespace-nowrap">{actionDetails}</p>
+              <p className="my-1 whitespace-nowrap">{part2}</p>
             </li>
           );
         })}
