20d9f7804ec32c49ad1d47305d92258bd798520b
X-Tripen <bnhp@op.pl>
2023-05-25 18:39:12 +0200

    feat:better scoreboard name logic

--- src/components/AppBox/AppBox-Scoreboard/Scoreboard/ScoreboardItem.tsx
+++ src/components/AppBox/AppBox-Scoreboard/Scoreboard/ScoreboardItem.tsx
@@ -10,13 +10,18 @@ function NavigationItem({ userName, score }: NavigationItemProps): JSX.Element {
   const scoreboardLanguage = useAppSelector(
     (state) => state.language.selectedLanguage.scoreboard
   );
+  const anonUsername = useAppSelector(
+    (state) => state.language.selectedLanguage.scoreboard.anonymousName
+  );
 
   return (
     <li className="px-5 py-2 bg-yellow-800 first:border-[#c89a30] first:border-2 bg-opacity-20 text-stone-300 hover:text-stone-50 duration-300 rounded-lg uppercase tracking-wider max-w-xl w-full flex sm:hover:scale-105 justify-between">
-      <p>
-        {scoreboardLanguage.name}
-        {userName}
-      </p>
+      <div className="flex">
+        {userName === "Anonymous" || (
+          <p className="pr-1.5">{scoreboardLanguage.name}</p>
+        )}
+        <p> {userName === "Anonymous" ? anonUsername : userName}</p>
+      </div>
       <p>
         {scoreboardLanguage.score}
         {score}
