a973781e8999940ceb6812838268899ae008bab7
X-Tripen <bnhp@op.pl>
2023-05-27 16:33:56 +0200

    refactor:add useMemo hook to navigation list map

--- src/components/AppBox/Navigation/NavigationList.tsx
+++ src/components/AppBox/Navigation/NavigationList.tsx
@@ -1,6 +1,8 @@
 import NavigationItem from "./NavigationItem";
 import { useAppSelector } from "../../../app/hooks";
 import { NavigationState } from "../../../app/slices/navigationSlice";
+import { useMemo } from "react";
+
 interface ListedNavigation extends NavigationState {
   readonly id: number;
   readonly name: string;
@@ -10,32 +12,39 @@ function NavigationList(): JSX.Element {
   const naviLanguage = useAppSelector(
     (state) => state.language.selectedLanguage.navigation
   );
-  const navigationList: ListedNavigation[] = [
-    {
-      id: 1,
-      name: naviLanguage.earnCookies,
-      link: "game",
-    },
-    {
-      id: 2,
-      name: naviLanguage.scoreboard,
-      link: "scoreboard",
-    },
-    {
-      id: 3,
-      name: naviLanguage.shop,
-      link: "shop",
-    },
-  ];
-  const renderedNavigationList = navigationList.map((navigation) => {
-    return (
-      <NavigationItem
-        key={navigation.id}
-        name={navigation.name}
-        link={navigation.link}
-      />
-    );
-  });
+  const navigationList: ListedNavigation[] = useMemo(
+    () => [
+      {
+        id: 1,
+        name: naviLanguage.earnCookies,
+        link: "game",
+      },
+      {
+        id: 2,
+        name: naviLanguage.scoreboard,
+        link: "scoreboard",
+      },
+      {
+        id: 3,
+        name: naviLanguage.shop,
+        link: "shop",
+      },
+    ],
+    [naviLanguage]
+  );
+
+  const renderedNavigationList = useMemo(
+    () =>
+      navigationList.map((navigation) => (
+        <NavigationItem
+          key={navigation.id}
+          name={navigation.name}
+          link={navigation.link}
+        />
+      )),
+    [navigationList]
+  );
+
   return (
     <div className="flex flex-col min-[500px]:flex-row justify-between bg-yellow-800 bg-opacity-20 rounded-lg m-4 xs:m-1">
       {renderedNavigationList}
