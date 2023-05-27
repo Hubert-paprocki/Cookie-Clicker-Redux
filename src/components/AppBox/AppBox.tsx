240129f8a3a7d367eaa8440e0f528c20808d27f5
X-Tripen <bnhp@op.pl>
2023-05-27 15:40:19 +0200

    refactor:improved readability for switch slice css

--- src/components/AppBox/AppBox.tsx
+++ src/components/AppBox/AppBox.tsx
@@ -8,17 +8,18 @@ import ScoreboardBox from "./AppBox-Scoreboard/ScoreboardBox";
 function AppBox(): JSX.Element {
   const slide = useAppSelector((state) => state.navigation.link);
 
+  const switchSlide =
+    slide === "shop"
+      ? "-translate-x-[66.6666%] min-[1024px]:-translate-x-[2048px]"
+      : slide === "scoreboard"
+      ? "-translate-x-[33.3333%] min-[1024px]:-translate-x-[1024px]"
+      : "";
+
   return (
-    <div className="w-full min-[1024px]:w-[1024px] bg-stone-800 rounded-none  overflow-hidden min-[1024px]:rounded-lg max-[500px]:min-h-screen ">
+    <div className="w-full min-[1024px]:w-[1024px] bg-stone-800 rounded-none overflow-hidden min-[1024px]:rounded-lg max-[500px]:min-h-screen">
       <NavigationList />
       <div
-        className={`flex w-[300%] min-[1024px]:w-[3072px] duration-300 ease-long-slide ${
-          slide === "shop" &&
-          " -translate-x-[66.666666666666%] min-[1024px]:-translate-x-[2048px] "
-        } ${
-          slide === "scoreboard" &&
-          " -translate-x-[33.3333333333333%] min-[1024px]:-translate-x-[1024px] "
-        }`}
+        className={`flex w-[300%] min-[1024px]:w-[3072px] duration-300 ease-long-slide ${switchSlide}`}
       >
         <GameBox />
         <ScoreboardBox />
