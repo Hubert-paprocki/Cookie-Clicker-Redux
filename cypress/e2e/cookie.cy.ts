dbad29cd37c7cec58c303a5ea09095ad8f59fb48
X-Tripen <bnhp@op.pl>
2023-05-30 09:28:44 +0200

    test: changed test to run on on development server instead poduction website

--- cypress/e2e/cookie.cy.ts
+++ cypress/e2e/cookie.cy.ts
@@ -1,10 +1,8 @@
 describe('Cookie Clicker App', () => {
   it('should increase the cookie count when the cookie is clicked', () => {
-    cy.visit('https://cookieclicker-hp.web.app/')
+    cy.visit('http://localhost:3000')
     cy.get('[data-testid="cookie-button"]').click()
     cy.get('[data-testid="cookie-number"]').should('have.text', '1');
-    cy.get('[data-testid="cookie-button"]').click()
-    cy.get('[data-testid="cookie-number"]').should('have.text', '2');
 
   })
 })
